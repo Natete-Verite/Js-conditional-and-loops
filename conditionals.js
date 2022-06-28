cae07b8be546dfb0b7ce006352f289bdf46b0e70
Natete_Verite <nateteverite@gmail.com>
2022-06-28 09:51:42 +0300

    Javascript conditionals and loops

---
+++ conditionals.js
@@ -0,0 +1,76 @@
+function integers() {
+    for (let n=0; n<=100; n++){
+        if (n%3===0 && n%5===0){
+            console.log("FizzBuzz");
+        }
+        else if (n%3===0){
+            console.log("Fizz");
+        }
+        else if (n%5===0){
+            console.log("Buzz");
+        }
+    }
+}
+integers()
+
+function multiples_sum(){
+    var sum = 0;
+    for (let n=0; n<1000; n++){
+        if (n%3===0 && n%5===0){
+            sum+=n 
+        }
+    }
+    console.log(sum);
+}   
+multiples_sum() 
+
+function odd_or_even(){
+    for (let n=0; n<=20; n++){
+        if (n%2===0){
+            console.log( n + " " + "is even");
+        }
+        else{
+            console.log( n + " " + "is odd");
+        }
+    }
+}
+odd_or_even()
+
+function largest_number(){
+    var num = [-2,4,-5,6,0];
+    var largest = num[0];
+    for (let i = 0; i < num.length; i++) {
+        if (largest < num[i]){
+            largest = num[i];
+        }
+    }
+    console.log(largest);  
+}
+largest_number()
+
+function greatest_number(){
+    var a = 10;
+    var b = 40;
+    if (a > b){
+        console.log(a + " " + "is the largest");
+    }
+    else if (a < b){
+        console.log(b + " " + "is the largest");
+    }
+    else{
+        console.log("They are equal");
+    }
+}
+greatest_number()
+
+function leap_year_checker(){
+    for (let an = 2000; an <= 2022; an++){
+        if(an%400===0 && an%100===0){
+           console.log(an + " " + "is a leap year");
+        }
+        else if(an%4===0){
+            console.log(an + " " + "is a leap year");
+        }
+    }
+}
+leap_year_checker()
---
+++ revision.js
@@ -0,0 +1,30 @@
+// function children(){
+//     if (true){
+//     var name = "Verite"
+//     var  fruit1 =  'apple'  ;   //exist in function  scope 
+//     const  fruit2 =  'banana'  ;   //exist in block  scope 
+//     let  fruit3 =  'strawberry'  ;   //exist in blo
+
+//     } 
+//     console.log(name);
+//     console.log(fruit1); 
+//     console.log(fruit2); 
+//     console.log(fruit3); 
+    
+// }
+// children()
+// function num(){ //parent scope
+//     var a = 2;
+//     function add(){ //child scope
+//         var b = 3;
+//         var c = a+b;
+//         console.log(c);
+//     }
+//     add()
+// }
+// num()
+// add(30);
+// function add(c){
+//     var a = 10;
+//     console.log(a+c);
+// }
