8a41a9f97115380bc0045b255c0ac2a327feabf5
Natete_Verite <nateteverite@gmail.com>
2022-06-28 17:02:39 +0300

    rectified version Javascript conditionals and loops

--- conditionals.js
+++ conditionals.js
@@ -38,7 +38,7 @@ odd_or_even()
 
 function largest_number(){
     var num = [-2,4,-5,6,0];
-    var largest = num[0];
+    var largest = 0;
     for (let i = 0; i < num.length; i++) {
         if (largest < num[i]){
             largest = num[i];
